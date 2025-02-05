827b32bc7ea47e85284a4a67aa19d68c03a3bcd1
MakFio <90612141+MakFio@users.noreply.github.com>
2025-02-05 21:07:20 +0300

    V0.12

--- Structure.py
+++ Structure.py
@@ -10,26 +10,25 @@ class Walls:
     def generate_walls(self):
         num_walls = random.randint(15, 30)
         for _ in range(num_walls):
-            x_center = random.randint(-self.width + 25, self.width - 25)
-            y_center = random.randint(-self.height + 25, self.height - 25)
-            radius = random.randint(3, 25)
+            x_center = random.randint(-self.width + 30, self.width - 30)  # Увеличили отступы
+            y_center = random.randint(-self.height + 30, self.height - 30)
+            radius = random.randint(5, 30)  # Увеличили диапазон радиуса
 
             walls_to_add = [
-                {'x': x_center - radius, 'y': y_center - 3, 'width': 30, 'height': 6},  # Горизонтальная стена 1
-                {'x': x_center + 40 - radius, 'y': y_center - 3, 'width': 30, 'height': 6},  # Горизонтальная стена 2
-                {'x': x_center - 6 - radius, 'y': y_center - 5, 'width': 6, 'height': 80},  # Вертикальная стена 1
-                {'x': x_center + 70 - radius, 'y': y_center - 5, 'width': 6, 'height': 80},  # Вертикальная стена 2
-                {'x': x_center - 6 - radius, 'y': y_center + 75, 'width': 82, 'height': 6}  # Горизонтальная стена 3
+                {'x': x_center - radius, 'y': y_center - 5, 'width': 40, 'height': 10},  # Горизонтальная стена 1
+                {'x': x_center + 50 - radius, 'y': y_center - 5, 'width': 40, 'height': 10},  # Горизонтальная стена 2
+                {'x': x_center - 8 - radius, 'y': y_center - 10, 'width': 10, 'height': 90},  # Вертикальная стена 1
+                {'x': x_center + 90 - radius, 'y': y_center - 10, 'width': 10, 'height': 90},  # Вертикальная стена 2
+                {'x': x_center - 8 - radius, 'y': y_center + 90, 'width': 100, 'height': 10}  # Горизонтальная стена 3
             ]
 
-            # Проверяем каждую стену на близость к существующим
             if all(not self.is_too_close(wall) for wall in walls_to_add):
                 self.walls.extend(walls_to_add)
 
     def is_too_close(self, new_wall):
         for wall in self.walls:
-            distance = math.sqrt((new_wall['x'] - wall['x']) ** 2 + (new_wall['y'] - wall['y']) ** 2)
-            if distance < 30:
+            distance = math.hypot(new_wall['x'] - wall['x'], new_wall['y'] - wall['y'])
+            if distance < 40:  # Увеличили минимальное расстояние
                 return True
         return False
 
